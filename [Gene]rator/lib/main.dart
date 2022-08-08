2d300dadff0ebe1a3a8f22d984ac80c7869cb47f
GUSTAVO NOVAES <ganb.tads@gmail.com>
2022-08-08 18:12:26 -0300

    Ex 02

--- [Gene]rator/lib/main.dart
+++ [Gene]rator/lib/main.dart
@@ -1,34 +1,109 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:niothian/widget/cardStatus.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: MyPage(),
-  ));
+  runApp(
+    const Home(),
+  );
 }
 
-class MyPage extends StatefulWidget {
-  const MyPage({Key? key}) : super(key: key);
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
-  @override
-  State<MyPage> createState() => _MyPageState();
-}
-
-class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        title: const Center(
-          child: Text("[Gene]rator"),
+    final ButtonStyle style =
+        TextButton.styleFrom(primary: Theme.of(context).colorScheme.onPrimary);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey.shade900,
+          title: Center(
+            child: Text("[Gene]rator"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: style,
+              onPressed: () {},
+              child: const Text('Juntar-se'),
+            ),
+            TextButton(
+              style: style,
+              onPressed: () {},
+              child: const Text('Sair'),
+            )
+          ],
         ),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('img/welcome.jpg'),
-            fit: BoxFit.cover,
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Row(
+            children: [
+              Container(
+                width: 200,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(10),
+                color: Colors.pink,
+                child: Center(
+                  child: Text("Sexualidade"),
+                ),
+              ),
+              Container(
+                width: 200,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(10),
+                color: Colors.red,
+                child: Center(
+                  child: Text("Vida"),
+                ),
+              ),
+              Container(
+                width: 200,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(10),
+                color: Colors.orange,
+                child: Center(
+                  child: Text("Cura"),
+                ),
+              ),
+              Container(
+                width: 200,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(10),
+                color: Colors.yellow,
+                child: Center(
+                  child: Text("Luz do Sol"),
+                ),
+              ),
+              Container(
+                width: 200,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(10),
+                color: Colors.green,
+                child: Center(
+                  child: Text("Natureza"),
+                ),
+              ),
+              Container(
+                width: 200,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(10),
+                color: Colors.blue,
+                child: Center(
+                  child: Text("Serenidade e Harmonia"),
+                ),
+              ),
+              Container(
+                width: 200,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(10),
+                color: Colors.purple,
+                child: Center(
+                  child: Text("Espírito"),
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- [Gene]rator/lib/widget/cardStatus.dart
+++ [Gene]rator/lib/widget/cardStatus.dart
@@ -14,12 +14,44 @@ class cardStatus extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
+    return Container(
+      color: Colors.blue,
+      child: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
         children: [
-          Container(child: Text(atributos)),
-          Container(child: Text(modificador.toString())),
-          Container(child: Text(valor.toString())),
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.red,
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '6',
+                      style: TextStyle(color: Colors.red[500], fontSize: 25),
+                    ),
+                    Text(
+                      'sep',
+                      style: TextStyle(color: Colors.red[500]),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.black,
+          )
         ],
       ),
     );
